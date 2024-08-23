dba6a86eafca49c5ac921ace9356ccd852589854
Süleyman Güzey <suleymanguzey590@gmail.com>
2024-08-23 20:01:57 +0300

    Implement Pythagorean Triples Calculation
    - Added code to find and print Pythagorean triples (i, j, c) where i, j, and c satisfy the condition i^2 + j^2 = c^2.
    - Included product of the triplet values in the output.
    - Improved variable names and output formatting for clarity.

---
+++ PythagoreanTriples/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = input.nextInt();
+
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = i + 1; j < n - i; j++) {
+                int c = n - (i + j);
+                if (Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(c, 2)) {
+                    System.out.println(i + "," + j + "," + c + "," + (i * j * c));
+                }
+            }
+        }
+    }
+}
